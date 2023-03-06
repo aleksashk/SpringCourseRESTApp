7f3f195ff7b72f4b32ec726cfc103823b24ac135
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-03-06 14:58:54 +0300

    create MyRestController

---
+++ src/main/java/com/philimonov/spring/rest/controller/MyRestController.java
@@ -0,0 +1,9 @@
+package com.philimonov.spring.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+}
