eeb4fcbc7386cda0f77bbf7e247e711bea50f104
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-03-06 16:16:18 +0300

    add new Employee to a DB

--- src/main/java/com/philimonov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/philimonov/spring/rest/controller/MyRestController.java
@@ -1,15 +1,13 @@
 package com.philimonov.spring.rest.controller;
 
 import com.philimonov.spring.rest.entity.Employee;
-import com.philimonov.spring.rest.exceptionHandling.EmployeeIncorrectData;
 import com.philimonov.spring.rest.exceptionHandling.NoSuchEmployeeException;
 import com.philimonov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,4 +35,9 @@ public class MyRestController {
         return employee;
     }
 
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 }
