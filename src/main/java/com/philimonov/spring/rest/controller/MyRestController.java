9eb35dcae7119eb316918b80181b690ffae0ace7
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-03-06 16:26:37 +0300

    handling an exception

--- src/main/java/com/philimonov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/philimonov/spring/rest/controller/MyRestController.java
@@ -51,6 +51,10 @@ public class MyRestController {
 
     @DeleteMapping("/employees/{id}")
     public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if(employee == null){
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database.");
+        }
         employeeService.deleteEmployee(id);
         return "Employee with id = " + id + " was deleted!";
     }
